491184289008af27f7569f7dc6eb14dbf5f6dd0f
rutatiina <rutatiina@ruginem.org>
2022-07-27 19:00:57 +0300

    update

---
+++ src/Database/Migrations/2022_07_27_081317_create_rg_inventory_purchases_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRgInventoryPurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('tenant')->create('rg_inventory_purchases', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            //>> default columns
+            $table->softDeletes();
+            $table->unsignedBigInteger('tenant_id');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            //<< default columns
+
+            //>> table columns
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->date('date');
+            $table->unsignedBigInteger('item_id');
+            $table->string('batch', 50);
+            $table->unsignedInteger('units');
+            $table->unsignedInteger('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('tenant')->dropIfExists('rg_inventory_purchases');
+    }
+}
---
+++ src/InventoryServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rutatiina\Inventory;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\ServiceProvider;
+
+class InvoiceServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        include __DIR__.'/routes/routes.php';
+        //include __DIR__.'/routes/api.php';
+
+        // $this->loadViewsFrom(__DIR__.'/resources/views', 'inventory');
+        $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
+
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //$this->app->make('Rutatiina\Inventory\Http\Controllers\InventoryController');
+    }
+}
